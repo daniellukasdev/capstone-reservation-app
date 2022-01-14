7527091c87bb6077846e85f2f82cc28803b57674
Daniel Lukas <dtlukasart@gmail.com>
2022-01-14 14:40:32 -0500

    added error handling for async/working list and list by date

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,27 @@
+const reservationsService = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+
+/**
+ * #######################  Middleware  #######################
+ */
+
+/**
+ * #########################  Routes  #########################
+ */
+
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const date = req.query.date;
+  if (date) {
+    res.json({ data: await reservationsService.listReservationsByData(date) });
+  } else {
+    res.json({ data: await reservationsService.list() });
+  }
 }
 
 module.exports = {
-  list,
+  list: [asyncErrorBoundary(list)]
 };
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,1 +1,28 @@
 const knex = require("../db/connection");
+
+
+/**
+ * #################  Table Builder for Specific Routes  #################
+ */
+
+// creates a table that includes reservations for a specific data
+function listReservationsByData(date) {
+    return knex("reservations as r")
+        .select("t.*")
+        .where({ "r.reservation_date": date });
+}
+
+
+/**
+ * #################  Table Builder for Standard Routes  #################
+ */
+
+function list() {
+    return knex("reservations").select("*");
+}
+
+
+module.exports = {
+    list,
+    listReservationsByData,
+}
