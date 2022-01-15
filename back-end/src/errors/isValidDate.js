2f1d271ffa013778e02a25b227cc97a1e705063d
Daniel Lukas <dtlukasart@gmail.com>
2022-01-15 09:57:53 -0500

    created time format validity check function

--- back-end/src/errors/isValidDate.js
+++ back-end/src/errors/isValidDate.js
@@ -1,11 +1,11 @@
-// checks if the date request is formatted correctly
+// checks if the reservation date value is a valid date format
 function isValidDate(req, res, next) {
     const { data: { reservation_date } } = req.body;
     const dateFormat = /\d\d\d\d-\d\d-\d\d/;
     if (!reservation_date.match(dateFormat)) {
         return next({
             status: 400,
-            message: "The reservation_date must be a valid data format 'YYYY-MM-DD",
+            message: "The reservation_date must be a valid date format 'YYYY-MM-DD'",
         });
     }
     next();
