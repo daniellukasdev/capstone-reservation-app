bc8f5bcc5c0b8f88c5deba5a92d8545b211ad759
Daniel Lukas <dtlukasart@gmail.com>
2022-01-15 12:00:16 -0500

    created validity check for people property being a number

---
+++ back-end/src/errors/isValidNumber.js
@@ -0,0 +1,12 @@
+function isValidNumber(req, res, next) {
+    const { data: { people } } = req.body;
+    if (!Number.isInteger(people)) {
+        return next({
+            status: 400,
+            message: "The number of people must be a number",
+        });
+    }
+    next();
+}
+
+module.exports = isValidNumber;
