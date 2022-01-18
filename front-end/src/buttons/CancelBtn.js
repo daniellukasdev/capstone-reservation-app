938849fe77dccffe7d3325a76156899b9f6b4997
Daniel Lukas <dtlukasart@gmail.com>
2022-01-18 11:06:51 -0500

    removed inline function from onClick

--- front-end/src/buttons/CancelBtn.js
+++ front-end/src/buttons/CancelBtn.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 export default function CancelBtn() {
     const history = useHistory();
     return (
-        <button onClick={() => history.goBack()}>
+        <button onClick={history.goBack}>
             Cancel
         </button>
     );
