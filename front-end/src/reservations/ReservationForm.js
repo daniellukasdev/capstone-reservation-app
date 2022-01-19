8553f4badd518a6aaa96c51b8d94a48318d81433
Daniel Lukas <dtlukasart@gmail.com>
2022-01-19 11:16:19 -0500

    attempting to create reservation on Tuesday displays error message

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import { createReservation } from "../utils/api";
 import CancelBtn from "../buttons/CancelBtn";
 import SubmitBtn from "../buttons/SubmitBtn";
+import ErrorAlert from "../layout/ErrorAlert";
 
 
 export default function ReservationForm() {
@@ -52,91 +53,93 @@ export default function ReservationForm() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <fieldset>
-                <div className="col">
-                    <label htmlFor="first_name">First Name</label>
-                    <input
-                        type="text"
-                        // className="form-control"
-                        id="first_name"
-                        name="first_name"
-                        value={formData.first_name}
-                        onChange={handleInputChange}
-                        required placeholder={"First Name"}
-                    />
-                </div>
-                <div className="col">
-                    <label htmlFor="last_name">Last Name</label>
-                    <input
-                        type="text"
-                        // className="form-control"
-                        id="last_name"
-                        name="last_name"
-                        value={formData.last_name}
-                        onChange={handleInputChange}
-                        required placeholder={"Last Name"}
-                    />
-                </div>
-                <div className="col">
-                    <label htmlFor="mobile_number">Mobile Number</label>
-                    <input
-                        type="text"
-                        // className="form-control"
-                        id="mobile_number"
-                        name="mobile_number"
-                        value={formData.mobile_number}
-                        onChange={handleInputChange}
-                        required placeholder={"Mobile Number"}
-                        // pattern="\d[0-9]{3}-\d[0-9]{3}-\d[0-9]{4}"
-                    />
-                </div>
-                <div className="col">
-                    <label htmlFor="date">Date</label>
-                    <input
-                        type="date"
-                        // className="form-control"
-                        id="reservation_date"
-                        name="reservation_date"
-                        value={formData.reservation_date}
-                        onChange={handleInputChange}
-                        required placeholder={"YYYY-MM-DD"} 
-                        pattern="\d{4}-\d{2}-\d{2}"
-                    />
-                </div>
-                <div className="col">
-                    <label htmlFor="time">Time</label>
-                    <input
-                        type="time"
-                        // className="form-control"
-                        id="reservation_time"
-                        name="reservation_time"
-                        value={formData.reservation_time}
-                        onChange={handleInputChange}
-                        required placeholder={"HH:MM"} 
-                        pattern="[0-9]{2}:[0-9]{2}"
-                    />
-                </div>
-                <div className="col">
-                    <label htmlFor="people">Number of People</label>
-                    <input
-                        type="number"
-                        // className="form-control"
-                        id="people"
-                        name="people"
-                        value={formData.people}
-                        onChange={handleInputChange}
-                        required min="1"
-                    />
-                </div>
-                <div className="col">
-                    <div>
-                    <CancelBtn />
-                    <SubmitBtn />
+        <div>
+            <ErrorAlert error={error} />
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <div className="col">
+                        <label htmlFor="first_name">First Name</label>
+                        <input
+                            type="text"
+                            // className="form-control"
+                            id="first_name"
+                            name="first_name"
+                            value={formData.first_name}
+                            onChange={handleInputChange}
+                            required placeholder={"First Name"}
+                        />
                     </div>
-                </div>
-            </fieldset>
-            
-        </form>
+                    <div className="col">
+                        <label htmlFor="last_name">Last Name</label>
+                        <input
+                            type="text"
+                            // className="form-control"
+                            id="last_name"
+                            name="last_name"
+                            value={formData.last_name}
+                            onChange={handleInputChange}
+                            required placeholder={"Last Name"}
+                        />
+                    </div>
+                    <div className="col">
+                        <label htmlFor="mobile_number">Mobile Number</label>
+                        <input
+                            type="text"
+                            // className="form-control"
+                            id="mobile_number"
+                            name="mobile_number"
+                            value={formData.mobile_number}
+                            onChange={handleInputChange}
+                            required placeholder={"Mobile Number"}
+                            // pattern="\d[0-9]{3}-\d[0-9]{3}-\d[0-9]{4}"
+                        />
+                    </div>
+                    <div className="col">
+                        <label htmlFor="date">Date</label>
+                        <input
+                            type="date"
+                            // className="form-control"
+                            id="reservation_date"
+                            name="reservation_date"
+                            value={formData.reservation_date}
+                            onChange={handleInputChange}
+                            required placeholder={"YYYY-MM-DD"} 
+                            pattern="\d{4}-\d{2}-\d{2}"
+                        />
+                    </div>
+                    <div className="col">
+                        <label htmlFor="time">Time</label>
+                        <input
+                            type="time"
+                            // className="form-control"
+                            id="reservation_time"
+                            name="reservation_time"
+                            value={formData.reservation_time}
+                            onChange={handleInputChange}
+                            required placeholder={"HH:MM"} 
+                            pattern="[0-9]{2}:[0-9]{2}"
+                        />
+                    </div>
+                    <div className="col">
+                        <label htmlFor="people">Number of People</label>
+                        <input
+                            type="number"
+                            // className="form-control"
+                            id="people"
+                            name="people"
+                            value={formData.people}
+                            onChange={handleInputChange}
+                            required min="1"
+                        />
+                    </div>
+                    <div className="col">
+                        <div>
+                        <CancelBtn />
+                        <SubmitBtn />
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
     );
 }
