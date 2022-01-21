5c859d63df01ef8d0d20476c2828cc59907fc8b2
Daniel Lukas <dtlukasart@gmail.com>
2022-01-21 10:37:16 -0500

    created base route for :/reservation_id

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -8,6 +8,14 @@ const router = require("express").Router();
 const controller = require("./reservations.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
+router
+    .route("/")
+    .get(controller.list)
+    .post(controller.create)
+    .all(methodNotAllowed);
+
+router
+    .route("/:reservation_id")
+    .all(methodNotAllowed);
 
 module.exports = router;
