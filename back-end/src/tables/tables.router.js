351096e77a66e7d1fd66251cafb46d6d530b0159
Daniel Lukas <dtlukasart@gmail.com>
2022-01-21 11:31:00 -0500

    defined route for PUT request

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -9,6 +9,11 @@ const controller = require("./tables.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router
+    .route("/:table_id/seat")
+    .put(controller.update)
+    .all(methodNotAllowed);
+
+router
     .route("/")
     .get(controller.list)
     .post(controller.create)
