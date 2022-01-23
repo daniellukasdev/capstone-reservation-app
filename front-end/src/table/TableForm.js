6e794546bac54a29cf1d279b68fb111efb31a177
Daniel Lukas <dtlukasart@gmail.com>
2022-01-23 12:29:48 -0500

    table form works with successful api call

--- front-end/src/table/TableForm.js
+++ front-end/src/table/TableForm.js
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { createTable } from "../utils/api";
 import CancelBtn from "../buttons/CancelBtn";
 import SubmitBtn from "../buttons/SubmitBtn";
 import ErrorList from "../layout/ErrorList";
 
 export default function TableForm() {
+  // initial values of input fields
+  const initialFormState = {
+    "table_name": "",
+    "capacity": "",
+  }
+
+  /**
+   * state management of form input fields and error
+   */
+   const [formData, setFormData] = useState({ ...initialFormState });
+   const [error, setError] = useState(null);
+
+   const history = useHistory();
+
+   /**
+   * functions to handle input changes
+   */
+  const handleInputChange = ({ target }) => {
+    if (target.id === "capacity") {
+      setFormData({
+        ...formData,
+        [target.id]: Number(target.value),
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [target.id]: target.value,
+      });
+    }
+  };
+
+  // sends POST request to api with formData
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const abortController = new AbortController();
+    try {
+      await createTable(formData, abortController.signal);
+      setFormData({...initialFormState});
+      history.push("/dashboard");
+    } catch (err) {
+      setError(err);
+    }
+    return () => abortController.abort();
+  }
+
   return (
     <div>
-      <div>{/* <ErrorList error={error} /> */}</div>
-      <form>
+      <div><ErrorList error={error} /></div>
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <div className="row">
             <div className="form-group col">
@@ -17,10 +64,11 @@ export default function TableForm() {
                 className="form-control"
                 id="table_name"
                 name="table_name"
-                // value={formData.table_name}
-                // onChange={handleInputChange}
+                value={formData.table_name}
+                onChange={handleInputChange}
                 required
                 placeholder={"Table Name"}
+                minLength={2}
               />
             </div>
             <div className="form-group col">
@@ -30,8 +78,8 @@ export default function TableForm() {
                 className="form-control"
                 id="capacity"
                 name="capacity"
-                // value={formData.people}
-                // onChange={handleInputChange}
+                value={formData.capacity}
+                onChange={handleInputChange}
                 required
                 min={1}
               />
@@ -40,7 +88,7 @@ export default function TableForm() {
           <div className="col">
             <div className="row">
               <div className="mr-1">
-                <CancelBtn />
+                <CancelBtn goBack={true} />
               </div>
               <div className="ml-1">
                 <SubmitBtn />
