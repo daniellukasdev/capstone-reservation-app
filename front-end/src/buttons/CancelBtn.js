abe00985325560e57aa7bc320a8fa5d7a5b241b0
Daniel Lukas <dtlukasart@gmail.com>
2022-01-23 12:28:25 -0500

    added prop for handling history

--- front-end/src/buttons/CancelBtn.js
+++ front-end/src/buttons/CancelBtn.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
-export default function CancelBtn() {
-    const history = useHistory();
-    return (
-        <button className="btn btn-secondary" onClick={() => history.push("/")}>
-            <span className="oi oi-x mr-1"></span>
-            Cancel
-        </button>
-    );
+export default function CancelBtn({ goBack = false }) {
+  const history = useHistory();
+  return (
+    <button
+      className="btn btn-secondary"
+      onClick={() => (goBack ? history.goBack() : history.push("/"))}
+    >
+      <span className="oi oi-x mr-1"></span>
+      Cancel
+    </button>
+  );
 }
