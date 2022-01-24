53a21944b193a25e7e5bcc51e4331ba890ec8096
Daniel Lukas <dtlukasart@gmail.com>
2022-01-24 10:24:12 -0500

    added conditional rendering for 'Availability' column

--- front-end/src/table/TableItem.js
+++ front-end/src/table/TableItem.js
@@ -5,12 +5,16 @@ export default function TableItem({ table }) {
         table_id,
         table_name,
         capacity,
+        reservation_id,
     } = table
     return (
         <tr key={table_id}>
             <td>{table_id}</td>
             <td>{table_name}</td>
             <td>{capacity}</td>
+            <td data-table-id-status={table_id}>
+                {reservation_id ? "Occupied" : "Free"}
+            </td>
         </tr>
     );
 }
