187000fb01ceac1f18772747a45a1a8c7b65526d
Daniel Lukas <dtlukasart@gmail.com>
2022-01-26 21:58:36 -0500

    refactored list and removed extra list function. Now lists reservationsnot finished.

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,68 +1,50 @@
 const knex = require("../db/connection");
 
-
 /**
  * #################  Table Builder for Specific Routes  #################
  */
-
-// creates a table that includes reservations for a specific data
-function listReservationsByDate(reservation_date) {
-    return knex("reservations as r")
-        .select("*")
-        .where({ reservation_date })
-        .orderBy("r.reservation_time");
-}
 
 
 /**
  * #################  Table Builder for Standard Routes  #################
  */
 
-// creates a table containing all reservations
-function list() {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    const today = year + "-" + month + "-" + day;
-
-    return knex("reservations")
+// creates a table that includes reservations for a specific date
+function list(date) {
+  return knex("reservations")
     .select("*")
-    .where({ reservation_date: today })
-    .orderBy("reservation_date");
+    .where({ reservation_date: date })
+    .whereNot({ status: "finished" })
+    .orderBy("reservation_time", "asc");
 }
 
 // creates a new row in the reservations table
 function create(newReservation) {
-    return knex("reservations")
-        .insert(newReservation)
-        .returning("*")
-        .then((createdReservations) => createdReservations[0]);
+  return knex("reservations")
+    .insert(newReservation)
+    .returning("*")
+    .then((createdReservations) => createdReservations[0]);
 }
 
 // creates a table for a single reservation
 function read(reservation_id) {
-    return knex("reservations")
-        .select("*")
-        .where({ reservation_id})
-        .first();
+  return knex("reservations").select("*").where({ reservation_id }).first();
 }
 
 // for the reservqtion with the given id, overrides the
 // status with the status param
 function updateStatus(reservation_id, status) {
-    return knex("reservations")
-        .where({ reservation_id })
-        .update({ status })
-        .returning("*")
-        .then((res) => res[0]);
+  return knex("reservations")
+    .where({ reservation_id })
+    .update({ status })
+    .returning("*")
+    .then((res) => res[0]);
 }
-
 
 module.exports = {
-    list,
-    listReservationsByDate,
-    read,
-    create,
-    updateStatus,
-}
+  list,
+  list,
+  read,
+  create,
+  updateStatus,
+};
