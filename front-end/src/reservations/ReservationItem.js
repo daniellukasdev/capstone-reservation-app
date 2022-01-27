a33e00bf2db851fd5b65b4e0325d0c4d688b7c80
Daniel Lukas <dtlukasart@gmail.com>
2022-01-27 09:12:44 -0500

    seat button only visible when status is 'booked'

--- front-end/src/reservations/ReservationItem.js
+++ front-end/src/reservations/ReservationItem.js
@@ -2,31 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function ReservationItem({ reservation }) {
-    const { 
-        reservation_id, 
-        first_name, 
-        last_name, 
-        mobile_number, 
-        reservation_date, 
-        reservation_time, 
-        people, 
-    } = reservation;
-    return (
-        <tr key={reservation_id}>
-            <td>{reservation_id}</td>
-            <td>{last_name}, {first_name}</td>
-            <td>{mobile_number}</td>
-            <td>{reservation_date}</td>
-            <td>{reservation_time}</td>
-            <td>{people}</td>
-            <td>
-                <Link 
-                    to={`/reservations/${reservation_id}/seat`}
-                    className="btn btn-secondary"
-                >
-                    Seat
-                </Link>
-            </td>
-        </tr>
-    );
+  const {
+    reservation_id,
+    first_name,
+    last_name,
+    mobile_number,
+    reservation_date,
+    reservation_time,
+    people,
+    status,
+  } = reservation;
+
+  const isBooked = status === "booked";
+  return (
+    <tr key={reservation_id}>
+      <td>{reservation_id}</td>
+      <td>
+        {last_name}, {first_name}
+      </td>
+      <td>{mobile_number}</td>
+      <td>{reservation_date}</td>
+      <td>{reservation_time}</td>
+      <td>{people}</td>
+      <td data-reservation-id-status={reservation_id}>{status}</td>
+      <td>
+        {isBooked ? (
+          <Link
+            to={`/reservations/${reservation_id}/seat`}
+            className="btn btn-secondary"
+          >
+            Seat
+          </Link>
+        ) : null}
+      </td>
+    </tr>
+  );
 }
