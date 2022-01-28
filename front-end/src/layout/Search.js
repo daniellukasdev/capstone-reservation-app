d22efa47d8f2f50f795ce59bc4a4dde3e15af424
Daniel Lukas <dtlukasart@gmail.com>
2022-01-28 09:43:29 -0500

    created form, input, and find button for search

--- front-end/src/layout/Search.js
+++ front-end/src/layout/Search.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import ErrorList from "./ErrorList";
 
 export default function Search() {
-    return (
-        <div>
-            <h1>Search Reservations</h1>
-        </div>
-    );
+  const [searchError, setSearchError] = useState(null);
+  return (
+    <div>
+      <div>
+        <h1>Search Reservations</h1>
+      </div>
+      <div>
+        <ErrorList error={searchError} />
+      </div>
+      <form>
+        <fieldset>
+          <div className="row">
+            <div className="form-group col">
+              <label htmlFor="mobile_number">Mobile Number:</label>
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="mobile_number"
+                  name="mobile_number"
+                  value={""}
+                  onChange={"handleInputChange"}
+                  required
+                  placeholder="Enter the customer's phone number."
+                />
+                <div className="input-group-append">
+                  <button className="btn btn-primary" type="submit">
+                    <span className="oi oi-magnifying-glass mr-1"></span>
+                    Find
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
